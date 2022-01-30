b342f7b977b116bce3a7fe92da6ea4b6ef671bbe
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-01-30 21:31:42 +0200

    Initial commit

---
+++ src/main/java/com/app/presidentialelectionapplication/PresidentialElectionApplication.java
@@ -0,0 +1,14 @@
+package com.app.presidentialelectionapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresidentialElectionApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PresidentialElectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/presidentialelectionapplication/PresidentialElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.presidentialelectionapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresidentialElectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
