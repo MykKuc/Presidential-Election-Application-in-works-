ee1f404d112e4b3d5e371c532e62e7f2eadf3408
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-01-31 11:52:15 +0200

    Create a Voter entity and add dao,rest,service packages

--- src/main/java/com/app/presidentialelectionapplication/entity/Voter.java
+++ src/main/java/com/app/presidentialelectionapplication/entity/Voter.java
@@ -1,4 +1,48 @@
 package com.app.presidentialelectionapplication.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="voters")
 public class Voter {
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private int id;
+    @Column(name="region")
+    private String region;
+    @Column(name="vote")
+    private String vote;
+
+    public Voter(){}
+
+    public Voter(String region, String vote) {
+        this.region = region;
+        this.vote = vote;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getVote() {
+        return vote;
+    }
+
+    public void setVote(String vote) {
+        this.vote = vote;
+    }
+
+    @Override
+    public String toString() {
+        return "Voter{" +
+                "id=" + id +
+                ", region='" + region + '\'' +
+                ", vote='" + vote + '\'' +
+                '}';
+    }
 }
